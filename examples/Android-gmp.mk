f6d7e33b719b3255cd7bf3c8e48527d077c1cd8a
android <trevd1234@gmail.com>
2014-05-04 05:18:42 +0100

    Some more simple examples for gmp, mpfr

---
+++ examples/Android-gmp.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_AUTOTOOLS_VARS)
+LOCAL_MODULE := gmp
+include $(BUILD_AUTOTOOLS)
---
+++ examples/Android-mpfr.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_AUTOTOOLS_VARS)
+LOCAL_MODULE := mpfr
+LOCAL_CFLAGS := -UHAVE_LOCALE_H
+include $(BUILD_AUTOTOOLS)
